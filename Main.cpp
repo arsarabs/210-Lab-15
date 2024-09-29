dbb30a30f1ca3b710f6e588c8c28e483ee589498
Your Name <your-email@example.com>
2024-09-29 03:31:11 -0700

    added two new functions (both prototypes and definitions). the readMovies() which reads from file, and output movies() which outputs contents from said file

--- Main.cpp
+++ Main.cpp
@@ -15,6 +15,9 @@ using namespace std;
 //For your container, you can choose an <array> class array or a <vector> class vector.
 //Store your four records in this container. Towards the end of your main() function, output the contents of the array / vector.
 
+//Define constant
+const string FILE_INPUT = "input.txt";
+
 //STEP #1: Coding a Movie class that has screen writer, the year released, and the title as its private member variables
 class Movie {
 
@@ -43,7 +46,11 @@ public:
 	void print() const;
 };
 
-//STEP #2: Function Definitions
+//STEP #3a: Function Prototypes for reading from file and outputting 
+bool readMovies(vector<Movie>& movies, const string& filename);
+void outputMovies(const vector <Movie>& movies);
+
+//STEP #2a: Function Definitions
 //Movie() default constructor
 Movie::Movie() : yearReleased(0) {}
 
@@ -52,8 +59,7 @@ Movie::Movie() : yearReleased(0) {}
 Movie::Movie(const string& writer, int year, const string& title): 
 	screenWriter(writer), yearReleased(year), movieTitle(title) {}
 
-//We're getting into the setters now
-
+//STEP #2b: Setter Definitions
 //setScreenWriter() sets writer's name, returns nothing, arugment is writer (string)
 void setScreenWriter(const string& writer) {
 	this->screenWriter = writer; 
@@ -62,14 +68,12 @@ void setScreenWriter(const string& writer) {
 void setYearReleased(const int year) {
 	this->yearReleased = year;
 }
-
 //setMovieTitle() sets movie title's name, returns nothing, arugment is title (string)
 void setMovieTitle(const string& movieTitle) {
 	this->movieTitle = movieTitle;
 }
 
-//GETTERS
-
+//STEP #2c: Getter Definitions
 //getScreenWriter() returns screen writer name, has no arguments
 string Movie::getScreenWriter() const {
 	return;
@@ -82,10 +86,25 @@ int Movie::getYearReleased() const {
 string Movie::getMovieTitle() const {
 	return;
 }
-
-//Print() outputs the detials of said movie, and it doesnt have any arguments nor does it return anything
+//STEP #2d: Print() outputs the detials of said movie, 
+//and it doesnt have any arguments nor does it return anything
 void Movie::print() const {
 	cout << "Screenwriter: " << screenWriter << endl;
 	cout << "Year released: " << yearReleased << endl;
 	cout << "Movie: " << movieTitle << endl;
 }
+
+//STEP #3b: Function Definitions for reading from file and outputting 
+bool readMovies(vector<Movie>& movies, const string& filename) {
+	ifstream inputFile(filename);
+
+	if (!inputFile) {
+		cout << "ERROR! Can't open file " << filename << endl;
+		return false;
+	}
+	string writer, title;
+	int year
+};
+void outputMovies(const vector <Movie>& movies) {
+
+};
